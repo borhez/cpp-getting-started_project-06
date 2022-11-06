0a2879d55250ed4484340bd3b07a8ba10f5cc22a
borhez <getlink32@gmail.com>
2022-11-07 01:01:29 +0300

    ex02 pointer_identify()

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -21,6 +21,24 @@ Base    *generate(void)
 	}
 }
 
+void	identify(Base* p)
+{
+    /*
+        dynamic_cast:
+        Нисходящее-приведение типа, от базового класса к потомку.
+        Проверка корректности производится сама,
+        при невозможности приведения, возвращает NULL
+    */
+	if (dynamic_cast<A*>(p))
+			std::cout << "pointer-identify: A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+			std::cout << "pointer-identify: B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+			std::cout << "pointer-identify: C" << std::endl;
+	else
+			std::cout << "It's no class" << std::endl;
+}	
+
 int main()
 {
     return 0;
