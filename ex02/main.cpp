2fc27f3a4e76fcdb5d1ae321e968d5a1675623e9
borhez <getlink32@gmail.com>
2022-11-07 00:43:20 +0300

    ex02 generate()

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,3 +1,25 @@
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include <cmath>
+#include <iostream>
+
+Base    *generate(void)
+{
+	switch (rand() % 3)
+	{
+		case 0:
+			std::cout << "Create class A" << std::endl;
+			return ((Base *)new A);
+		case 1:
+			std::cout << "Create class B" << std::endl;
+			return ((Base *)new B);
+		default:
+			std::cout << "Create class C" << std::endl;
+			return ((Base *)new C);
+	}
+}
 
 int main()
 {
