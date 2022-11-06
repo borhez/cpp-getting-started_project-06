224e185cc5c4599104547d5a54985ecf800b3bba
borhez <getlink32@gmail.com>
2022-11-06 16:22:35 +0300

    Char.cpp

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,7 +1,18 @@
+#include <iostream>
 
-
-int main(void)
+int	main(int argc, char **argv)
 {
+	if (argc != 2)
+	{
+		std::cerr << "Usage: ./converter <literal>" << std::endl;
+		return (1);	
+	}
+	std::string str(argv[1]);
+
+	Char	c(str);
+	Double	d(str);
+	Float	f(str);
+	Int		i(str);
 	
 	return 0;
 }
